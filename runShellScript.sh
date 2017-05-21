0684f8d2146b1724bc1e8d19f34378060314601d
Beasta <barryblaha@gmail.com>
2017-05-20 22:40:15 -0700

    Make logging more explanatory

--- runShellScript.sh
+++ runShellScript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "giving $1 execute permission ... chmod +x $1"
+echo "giving $1 execute permission"
+echo "chmod +x $1"
 chmod +x $1
-echo "executing $1 ... ./$1"
+echo "executing $1"
+echo "./$1"
 ./$1
